4fc4ec7b7a7003df84ae757ac4ab32651cf99772
Gerardo Benitez <gerardo.benitez@ayi.group>
2025-02-21 17:02:48 -0300

    change log position to /tmp

--- app/core/logger.py
+++ app/core/logger.py
@@ -15,11 +15,12 @@ console_handler = logging.StreamHandler(sys.stdout)
 console_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
 
-
-os.makedirs("logs", exist_ok=True)
+log_dir = "/tmp/logs"
+os.makedirs(log_dir, exist_ok=True)
+log_file = os.path.join(log_dir, "app.log")
 
 
 # Manager to write logs in file
-file_handler = RotatingFileHandler("logs/app.log", maxBytes=1000000, backupCount=3, delay=True)
+file_handler = RotatingFileHandler(filename=log_file, maxBytes=1000000, backupCount=3, delay=True)
 file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)

2538fa695c412880ee863f65e264fcfe4b1acf3b
Gerardo Benitez <gerardobenitez@gmail.com>
2026-02-06 09:31:35 -0300

    cambio en la configuracion de pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,14 @@
 module.exports = {
-  apps : [{
-    name: "match-combat",
-    script: "./main.py",
-    cwd: "/root/projects/match_combat/app/", // PM2 saltará aquí antes de ejecutar el script
-    interpreter: "/root/projects/match_combat/.venv/bin/python"
-  }]
-}
+    apps : [{
+      name: "match-combat",
+      // Apuntamos al binario de uvicorn en el venv
+      script: "./venv/bin/uvicorn",
+      // Pasamos los argumentos como un array
+      args: "app.main:app --port 9009",
+      // Opcional: forzamos el intérprete del venv
+      interpreter: "./venv/bin/python",
+      instances: 1,
+      autorestart: true,
+      watch: false
+    }]
+  }

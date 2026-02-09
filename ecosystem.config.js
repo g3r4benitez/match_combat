ef9d898c868f2157ce569efd8b5eafd826873417
Gerardo Benitez <gerardobenitez@gmail.com>
2026-02-09 17:07:38 -0300

    para todo los hosts

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
       // Apuntamos al binario de uvicorn en el venv
       script: "./.venv/bin/uvicorn",
       // Pasamos los argumentos como un array
-      args: "app.main:app --port 9009",
+      args: "app.main:app --host 0.0.0.0 --port 9009",
       // Opcional: forzamos el intérprete del venv
       interpreter: "./.venv/bin/python",
       instances: 1,
